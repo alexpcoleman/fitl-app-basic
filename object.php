1d3d750ef242597d13eb322959e98aba97274f39
Alex Coleman <me@alexpcoleman.com>
2015-06-25 12:40:58 -0500

    created a database-powered dynamic object page!

--- object.php
+++ object.php
@@ -3,36 +3,47 @@ $id = $_REQUEST['id'];
 
 $object = array(
 	'title' => '',
-	'question' => '',
 	'description' => '',
 	'code' => '',
-	'date' => '',
+	'submitted_at' => '',
 );
 
-// set the object variables
-// based on the id value from the URL
-if ($id == 1) {
-	$object = array(
-		'title' => 'Programming Question #1',
-		'question' => 'I\'m having trouble displaying a JavaScript alert.',
-		'description' => 'I think I\'m using the correct function, but I can\'t figure out what\'s wrong. Could you point me in the right direction?',
-		'code' => 'alert(This is a message)',
-		'date' => 'June 11, 2015',
-	);
+
+// Database connection credentials
+$servername = 'localhost';
+$username = 'homestead';
+$password = 'secret';
+
+// Create connection
+$connection = new mysqli($servername, $username, $password);
+
+// Check for an error
+if ($connection->connect_error) {
+	echo 'Connection failed: ' . $connection->connect_error;
+	exit;
 }
-elseif ($id == 2) {
-	$object = array(
-		'title' => 'Programming Question #2',
-		'question' => 'My HTML list isn\'t displaying properly.',
-		'description' => 'I think I\'m using the write list element, but it\'s not showing up correctly. Any thoughts?',
-		'code' => '&lt;ul&gt;
-			item 1
-			item 2
-			item 3
-		&lt;/ul&gt;',
-		'date' => 'June 16, 2015',
-	);
+
+// Otherwise, connected successfully!
+// echo 'Connected successfully!';
+
+// Connect to the "fitl" database
+$connection->select_db('fitl');
+
+// Query to select the object
+// SELECT * FROM questions WHERE id = 1
+$sql = 'SELECT * FROM questions WHERE id = ' . $id;
+
+// Execute the query
+$result = $connection->query($sql);
+
+// Check for and retrieve the object
+if ($result->num_rows > 0) {
+	$object = $result->fetch_assoc();
+	// echo '<pre>';
+	// print_r($object);
+	// echo '</pre>';
 }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -40,11 +51,11 @@ elseif ($id == 2) {
 		<title><?php echo $object['title']; ?></title>
 	</head>
 	<body>
-		<h1><?php echo $object['question']; ?></h1>
+		<h1><?php echo $object['title']; ?></h1>
 		<p><?php echo $object['description']; ?></p>
 		<pre>
 			<?php echo $object['code']; ?>
 		</pre>
-		<p>Question Date: <?php echo $object['date']; ?></p>
+		<p>Question submitted_at: <?php echo $object['submitted_at']; ?></p>
 	</body>
 </html>
